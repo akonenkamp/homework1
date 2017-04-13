fecb7b2aa8be9f20b22194474b958892c014319a
akonenkamp <akonenkamp@gmail.com>
2017-04-13 12:42:32 -0700

    code

---
+++ math.js
@@ -0,0 +1,21 @@
+var inputs = [];
+var sum = 0;
+var avg = 0;
+function addNumber(event) {
+  var formNumber = document.mathForm.formNumber.value;
+  var number = Number(formNumber);
+  if(!isNaN(number))  {
+    inputs.push(number);
+    sum += number;
+    avg = sum/inputs.length;
+    document.getElementById("count").innerHTML = inputs.length;
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("avg").innerHTML = avg;
+    console.log(inputs);
+    console.log(sum);
+    console.log(avg);  
+  }
+  event.preventDefault();
+}
+
+
