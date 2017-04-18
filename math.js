61e5815b31bbd90765d64e1386c8658624453cd5
akonenkamp <akonenkamp@gmail.com>
2017-04-17 21:55:44 -0700

    homework updatE

--- math.js
+++ math.js
@@ -1,21 +1,38 @@
 var inputs = [];
 var sum = 0;
 var avg = 0;
+var string = 0;
+var count = 0;
+var amountaccum = "";
 function addNumbers(event) {
-  var formNumbers = document.mathForm.formNumbers.value;
-  var numbers = Number(formNumbers);
+  var textarea = $('#formNumbers').val();
+  console.log(textarea);
+  var numbers = Number(textarea);
   if(!isNaN(numbers))  {
     inputs.push(numbers);
     sum += numbers;
     avg = sum/inputs.length;
-    document.getElementById("counting").innerHTML = inputs.length;
-    document.getElementById("sums").innerHTML = sum;
-    document.getElementById("avg").innerHTML = avg;
+    count++;
     console.log(inputs);
     console.log(sum);
     console.log(avg);  
+    console.log(count);
+    $("#counting").html(count);
+    $("#sums").html(sum);
+    $("#avg").html(avg);
+   
+  } else {
+    inputs.push(textarea);
+    $("#string").html(inputs.length);
+    console.log(inputs.length);
+    amountaccum = amountaccum + textarea;
+    console.log(amountaccum);
+    $("#amountaccum").html(amountaccum);
+    
+    
   }
+  
   event.preventDefault();
 }
 
-document.getElementById("mathForm")
+//document.getElementById("reset").reset();
